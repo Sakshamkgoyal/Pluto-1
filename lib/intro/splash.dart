86aeacaecb097f6a1c41e1b72056445580003e56
Saksham Goyal <sakshamgoyal278@gmail.com>
2020-11-22 17:30:12 +0530

    splash

---
+++ lib/intro/splash.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+import 'package:pluto/intro/intro.dart';
+
+class splash extends StatefulWidget {
+  @override
+  _splashState createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+
+  String _animationName = "splash";
+  @override
+
+  void initState(){
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+        () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>intro())),
+    );
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff181818),
+      body: Container(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            FlareActor(
+              "assets/splash.flr",
+              fit: BoxFit.cover,
+              alignment: Alignment.center,
+              animation: _animationName,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pluto/intro/intro.dart';
+import 'package:pluto/intro/splash.dart';
 
 
 void main() {
@@ -11,12 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child){
+        return ScrollConfiguration(
+          behavior: MyBehavior(),
+          child: child,
+        );
+      },
       theme: ThemeData(fontFamily: 'em'),
       initialRoute: "home",
       routes: {
-        "home" : (context)  => intro(),
+        "home" : (context)  => splash(),
     },
   debugShowCheckedModeBanner: false,
     );
+  }
+}
+
+class MyBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
