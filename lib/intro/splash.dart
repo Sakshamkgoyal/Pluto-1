4d5893033ed09eae9da73446cd8b557fe51831b8
Saksham Goyal <sakshamgoyal278@gmail.com>
2020-11-23 12:43:14 +0530

    splash

--- lib/intro/splash.dart
+++ lib/intro/splash.dart
@@ -17,7 +17,7 @@ class _splashState extends State<splash> {
   void initState(){
     super.initState();
     Timer(
-      Duration(seconds: 3),
+      Duration(seconds: 4),
         () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>intro())),
     );
   }
